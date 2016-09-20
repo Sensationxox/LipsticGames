1147c4bf2fdb8bef5a64ef53c35571767bc7ae0e
MortaPlyPly <gupijazaure@gmail.com>
2016-09-20 17:54:08 +0300

    startiniai asetai 1
    MovingCamera
    StillCamera
    GameController
    Triggers/Spawners
    Canvas(shitty)

---
+++ Assets/Scripts/CameraResoliutionScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class CameraResoliutionScript : MonoBehaviour {
+
+    public GameObject hiddenMeniu;
+    public GameObject hiddenScene;
+    public GameObject meniuButton;
+    public float speed = 100f;
+    private Vector2 vector = new Vector2(1,0);
+    public bool fight = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("EnemySpawnPoint"))
+        {
+            fight = true;
+        }
+        if (other.gameObject.CompareTag("Stop"))
+        {
+            speed = 0f;
+            hiddenScene.transform.position = new Vector2(240, 150);
+        }
+        Destroy(other.gameObject);
+    }
+    
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = vector.normalized * speed * Time.deltaTime;
+    }
+
+    ////canvas buttons & etc
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowHiddenMeniu()
+    {
+        hiddenMeniu.transform.position = new Vector2(240, 150);
+        meniuButton.transform.position = new Vector2(500, 500);
+        speed = 0f;
+        //char speed = 0f;
+    }
+
+    public void HideHiddenMeniu()
+    {
+        hiddenMeniu.transform.position = new Vector2(850, 500);
+        meniuButton.transform.position = new Vector2(240, 150);
+        speed = 100f;
+        //char speed = 100f;6
+    }
+    
+}
---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+
+    public new Camera camera;
+
+    void Start () {
+	
+	}
+
+	void Update () {
+        if (camera.GetComponent<CameraResoliutionScript>().fight)
+        {
+            camera.GetComponent<CameraResoliutionScript>().fight = false;
+            camera.GetComponent<CameraResoliutionScript>().speed = 0f;
+            //initiate fight: spawn enemies & etc....
+            StartCoroutine(Wait()); //this is fight ... write if (fight.End == true) { camera speed = 100f}
+        }
+	}
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5); //this is not after 10 seconds, but after fight
+        camera.GetComponent<CameraResoliutionScript>().speed = 100f;
+    }
+}
